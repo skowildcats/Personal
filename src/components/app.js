7a693a99dda7b14cc59a9072fc466f358e284546
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-12 00:43:24 -0500

    loading page setup

--- src/components/app.js
+++ src/components/app.js
@@ -4,20 +4,43 @@ import NavBar from "./nav_bar"
 import AboutMe from './about_me';
 import Skills from './skills';
 
-const App = () => (
-  <div id="app">
-    {/* <NavBar/> */}
-    <div id="body">
-      <AboutMe />
-      <Skills/>
-      <div id="projects">
-        Projects
-      </div>
-      <div>
-        Contact
-      </div>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {loading: true}
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false})
+    }, 5000);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div>
+          Hi
+        </div>
+      )
+    } else {
+      return (
+        <div id="app">
+          {/* <NavBar/> */}
+          <div id="body">
+            <AboutMe />
+            <Skills/>
+            <div id="projects">
+              Projects
+            </div>
+            <div>
+              Contact
+            </div>
+          </div>
+        </div>
+      )
+    }
+  }
+};
 
 export default App;
