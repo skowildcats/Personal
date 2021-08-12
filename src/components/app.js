5cf3c7881f190392b01eea29641514e1d4b0ab7b
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-11 19:49:15 -0500

    icons

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,14 @@ import React from 'react';
 import "../stylesheets/application.scss";
 import NavBar from "./nav_bar"
 import AboutMe from './about_me';
+import Skills from './skills';
 
 const App = () => (
   <div id="app">
-    <NavBar/>
+    {/* <NavBar/> */}
     <div id="body">
       <AboutMe />
-      <div id="skills">
-        Skills
-      </div>
+      <Skills/>
       <div id="projects">
         Projects
       </div>
---
+++ src/components/skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SiRails, SiReact, SiRedux, SiPython, SiRuby, SiHtml5, SiCss3} from 'react-icons/si'
+
+class Skills extends React.Component {
+
+  render() {
+    return (
+      <div id="skills">
+        <SiRails />
+        <SiReact />
+        <SiRedux />
+        <SiPython />
+        <SiRuby />
+        <SiHtml5 />
+        <SiCss3 />
+      </div>
+    )
+  }
+}
+
+export default Skills
