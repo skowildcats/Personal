af81b54c751d9c468058992cba0f81cdfa0f4c20
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-12 14:58:01 -0500

    styling

--- src/components/app.js
+++ src/components/app.js
@@ -28,7 +28,7 @@ class App extends React.Component {
         <div id="app">
           <NavBar/>
           <div id="body">
-            <div id="header"></div>
+            {/* <div id="header"></div> */}
             <div id="footer"></div>
             <AboutMe />
             <Skills/>
