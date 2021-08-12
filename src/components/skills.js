54983f401910e79786d4af4965d02732b8e426cb
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-11 20:21:08 -0500

    spinning

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {SiRails, SiReact, SiRedux, SiPython, SiRuby, SiHtml5, SiCss3} from 'react-icons/si'
+import {SiRails, SiReact, SiRedux, SiPython, SiRuby, SiHtml5, SiCss3, SiPostgresql,
+  SiJavascript, SiNpm, SiJquery, SiGithub, SiMongodb, SiNodeDotJs} from 'react-icons/si'
 
 class Skills extends React.Component {
 
   render() {
     return (
       <div id="skills">
-        <SiRails />
-        <SiReact />
-        <SiRedux />
-        <SiPython />
-        <SiRuby />
-        <SiHtml5 />
-        <SiCss3 />
+        <div id="skills-text">
+          Hello
+        </div>
+        <div id="icon-container">
+          <SiRuby />
+          <SiRails />
+          <SiReact />
+          <SiRedux />
+          <SiPython />
+          <SiHtml5 />
+          <SiCss3 />
+          <SiPostgresql />
+          <SiJavascript />
+          <SiJquery />
+          <SiMongodb />
+          <SiNpm />
+          <SiGithub />
+          <SiNodeDotJs />
+        </div>
       </div>
     )
   }
