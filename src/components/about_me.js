2c51c517c3b4e63b81cabb67741379b62bb7111c
Jon Chen <jonathan.chen.6799@gmail.com>
2021-08-12 23:14:40 -0500

    refactor

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -39,11 +39,13 @@ class AboutMe extends React.Component {
               <img id="profile-pic" src={process.env.PUBLIC_URL + "/headshot.png"} alt="headshot" />
             </div>
             <div>
-              <span>Hi, I'm Jon </span>
-              <br/>
-              I'm a full stack software engineer with a passion for problem solving.
-              Looking to make an impact through beautiful and user-centric products.
-              Learn about some of my <span><a href="#skills">skills</a></span> and <span><a href="#projects">projects</a></span> below.
+              <div id="text-container">
+                <span>Hi, I'm Jon </span>
+                <br/>
+                I'm a full stack software engineer with a passion for problem solving.
+                Looking to make an impact through beautiful and user-centric products.
+                Learn about some of my <span><a href="#skills">skills</a></span> and <span><a href="#projects">projects</a></span> below.
+              </div>
             </div>
           </div>
         </div>
